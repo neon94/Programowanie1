d1da63b7e26bada87b89c38c4327f2d7aede86b7
Dawid Jaloza <dawidjaloza@gmail.com>
2018-10-12 12:50:21 +0200

    forth commit

--- src/main/java/StrongNumber/BinaryNumberConversion.java
+++ src/main/java/StrongNumber/BinaryNumberConversion.java
@@ -1,4 +1,32 @@
 package StrongNumber;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 public class BinaryNumberConversion {
+
+    public static void main(String[] args) {
+
+        List<Integer> komora = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Podaj liczbę do zamiany na binarną:");
+        int liczba = sc.nextInt();
+        System.out.println("w systemie binarnym to:");
+        System.out.println(binary(liczba));
+
+
+
+
+
+
+    }
+
+    private static int binary(int n) {
+            if (n == 0) return 0;
+            if (n % 2 == 0) System.out.println(0);
+            if (n % 2 != 0) System.out.println(1);
+            return binary(n/2);
+    }
 }
--- src/main/java/StrongNumber/SumRecuNumbers.java
+++ src/main/java/StrongNumber/SumRecuNumbers.java
@@ -1,4 +1,6 @@
 package StrongNumber;
 
 public class SumRecuNumbers {
+
+
 }
