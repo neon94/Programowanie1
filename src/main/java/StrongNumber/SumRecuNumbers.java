81de24836c703900eac19298b58f18b6f7bbc0ac
Dawid Jaloza <dawidjaloza@gmail.com>
2018-10-12 13:02:58 +0200

    forth commit

--- src/main/java/StrongNumber/SumRecuNumbers.java
+++ src/main/java/StrongNumber/SumRecuNumbers.java
@@ -1,6 +1,26 @@
 package StrongNumber;
 
+import java.util.Scanner;
+
 public class SumRecuNumbers {
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Podaj liczbe z ktorej ma byc policzona suma: ");
+        int liczba = sc.nextInt();
+        System.out.println("Suma rekurencyjna wynosi: " + recuSum(liczba));
+
+
+    }
+
+
+    private static int recuSum(int n) {
+
+        if (n > 0) {
+            n = n % 10 + recuSum(n/10);
+        }
+
+        return n;
+    }
 
 }
